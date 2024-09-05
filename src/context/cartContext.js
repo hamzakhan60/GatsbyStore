b6a326e84351f39e0fd8a724eb63a817f1587b83
Hamza Khan <124522006+hamzakhan60@users.noreply.github.com>
2024-09-06 04:00:06 +0500

    Update cartContext.js

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -35,3 +35,12 @@ export const CartProvider = ({ children }) => {
 };
 
 
+// Custom Hook to use Cart Context
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
